2eb4ed7061bb49cee0975e1015f1ab71388f2c2d
WebPossum <sandor.papp93@gmail.com>
2026-09-11 17:47:03 +0200

    Remove unnecessary parameter from index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { activeDrawing: false });
+    res.render("index.ejs");
 });
 
 app.get("/resume", (req, res) => {
